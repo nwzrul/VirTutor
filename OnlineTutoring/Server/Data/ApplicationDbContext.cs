545086ba19baaa4d1d32d295b75fa3e81ab0d65a
NAZRUL SYARIDZUAN BIN FAUZI 2204794H <2204794H@student.tp.edu.sg>
2023-12-10 12:21:41 +0800

    Add project files.

---
+++ OnlineTutoring/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OnlineTutoring.Server.Models;
+
+namespace OnlineTutoring.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ OnlineTutoring/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineTutoring.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
